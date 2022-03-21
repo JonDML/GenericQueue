2e626dbe256b57a1070d8a7a984400760e076a6d
JonDML <jon.de.miguel@gmail.com>
2022-03-21 22:31:21 +0100

    First commit

---
+++ genericqueue.h
@@ -0,0 +1,75 @@
+#ifndef GENERIC_QUEUE_H
+#define GENERIC_QUEUE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdatomic.h>
+
+#define GENERATE_QUEUE(TYPE) \
+struct Node_##TYPE{ \
+    TYPE value; \
+    struct Node_##TYPE *next; \
+}; \
+\
+typedef struct { \
+    struct Node_##TYPE *front, *rear; \
+    atomic_int size; \
+}Queue_##TYPE;\
+\
+int Queue_push_##TYPE(Queue_##TYPE* q, TYPE val) \
+{\
+    if(NULL == q) return -1; \
+    struct Node_##TYPE *tempNode = malloc(sizeof *tempNode); \
+    if(NULL == tempNode) return -2; \
+    tempNode->value = val; \
+    tempNode->next = NULL; \
+    if(NULL == q->rear) \
+    {\
+        q->front = q->rear = tempNode; \
+        ++q->size; \
+        return 0; \
+    }\
+    \
+    q->rear->next = tempNode; \
+    q->rear = tempNode; \
+    ++q->size; \
+    return 0; \
+} \
+\
+int Queue_pop_##TYPE(Queue_##TYPE* q, TYPE* val) \
+{\
+    if(NULL == q) return -1;\
+    if(NULL == q->front) return -3; \
+    struct Node_##TYPE* tempNode = q->front; \
+    q->front = q->front->next; \
+    \
+    if(NULL == q->front) q->rear = NULL; \
+    if(NULL != val) *val = tempNode->value; \
+    free(tempNode);\
+    --q->size; \
+    return 0; \
+}\
+\
+Queue_##TYPE* create_queue_##TYPE() \
+{\
+    Queue_##TYPE* q = malloc(sizeof *q); \
+    if(NULL == q) return NULL;\
+    q->front = q->rear = NULL; \
+    q->size = 0; \
+    return q; \
+}\
+\
+int destroy_queue_##TYPE(Queue_##TYPE* q) \
+{\
+    if(NULL == q) return -1;\
+    while(NULL != q->front) Queue_pop_##TYPE(q, NULL);\
+    free(q); \
+    q = NULL; \
+    return 0; \
+}\
+\
+int Queue_size_##TYPE(Queue_##TYPE *q) \
+{\
+    if(NULL == q) return -1; \
+    return q->size; \
+}
+#endif //GENERIC_QUEUE_H
---
+++ testqueue.c
@@ -0,0 +1,19 @@
+#include "genericqueue.h"
+
+int main()
+{
+    GENERATE_QUEUE(int)
+    
+    Queue_int *queue = NULL;
+    queue = create_queue_int();
+    Queue_push_int(queue, 5);
+    Queue_push_int(queue, 7);
+    printf("Queue size: %d\n", Queue_size_int(queue));
+    int num;
+    Queue_pop_int(queue, &num);
+    printf("First number: %d. Queue size: %d\n", num, Queue_size_int(queue));
+    Queue_pop_int(queue, &num);
+    printf("Second number: %d. Queue size: %d\n", num, Queue_size_int(queue));
+    destroy_queue_int(queue);
+    return 0;
+}
